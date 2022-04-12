069ba56cab19faea335ec13c752137ab55c23de7
mero2online <mero2online@gmail.com>
2022-04-12 08:36:04 +0200

    ADD: inputPathBox and outputPathBox and right click menu

--- GDR_Handler.py
+++ GDR_Handler.py
@@ -1,6 +1,8 @@
 from tkinter import filedialog
 from tkinter import *
 from tkinter import messagebox
+from tkinter import scrolledtext
+import pyperclip
 import asyncio
 import os
 import shutil
@@ -21,6 +23,42 @@ filetypes = (
 )
 
 
+def selectAll(*event):
+    inputPathBox.tag_add(SEL, "1.0", END)
+    inputPathBox.mark_set(INSERT, "1.0")
+    inputPathBox.see(INSERT)
+
+    outputPathBox.tag_add(SEL, "1.0", END)
+    outputPathBox.mark_set(INSERT, "1.0")
+    outputPathBox.see(INSERT)
+
+    return "break"
+
+
+def copy(*event):
+    try:
+        text = inputPathBox.selection_get()
+        text = outputPathBox.selection_get()
+        pyperclip.copy(text)
+    except TclError:
+        pass
+    return "break"
+
+
+def addToInputPathBox(txt):
+    inputPathBox.config(state="normal")
+    inputPathBox.delete('1.0', END)
+    inputPathBox.insert(INSERT, txt)
+    inputPathBox.config(state="disabled")
+
+
+def addToOutputPathBox(txt):
+    outputPathBox.config(state="normal")
+    outputPathBox.delete('1.0', END)
+    outputPathBox.insert(INSERT, txt)
+    outputPathBox.config(state="disabled")
+
+
 def createDocxReport():
     asyncio.run(createReport())
 
@@ -30,6 +68,8 @@ async def createReport():
     await HandleExcel(input_file, input_file_extension.get())
     if os.path.isfile(resource_path('output\\GEOLOGICAL_DESCRIPTION_REPORT.docx')):
         saveBtn.config(state='normal')
+        messagebox.showinfo(
+            'Success', f'GEOLOGICAL DESCRIPTION REPORT\nCreated successfully')
 
 
 def browseFile():
@@ -40,8 +80,9 @@ def browseFile():
 
     clearFiles()
     if (filename):
-        selectedFilePath.set(filename)
-        filenameOnly, file_extension = os.path.splitext(filename)
+        pathOnly, file_extension = os.path.splitext(filename)
+        selectedFilePath.set(pathOnly.split('/')[-1])
+        addToInputPathBox(filename)
         res = checkInputFile(filename, file_extension)
         if (res == 'LITHOLOGY DESCRIPTION SHEET'):
             createReportBtn.config(state="normal")
@@ -54,10 +95,14 @@ def browseFile():
             messagebox.showerror(
                 'File error', 'Please load valid LITHOLOGY DESCRIPTION SHEET')
             selectedFilePath.set('')
+            addToInputPathBox('')
+            addToOutputPathBox('')
             setButtonsDisabled()
 
     else:
         selectedFilePath.set('')
+        addToInputPathBox('')
+        addToOutputPathBox('')
         input_file_extension.set('')
         setButtonsDisabled()
 
@@ -74,9 +119,15 @@ async def saveAllFiles(filename):
         date = getFinalWellDate()
         time = getTimeNowText()
         dest_dir = f'{filename}/GDR-Output-{date}-{time}'
+        addToOutputPathBox(dest_dir)
         await copyFiles(src_files, dest_dir)
-        messagebox.showinfo(
-            'Success', f'Files saved successfully to\n{dest_dir}')
+        result = messagebox.askquestion(
+            'Success', f'Files saved successfully\n\nOpen output folder?')
+        if result == 'yes':
+            opd = os.getcwd()  # Get original directory
+            os.chdir(dest_dir)  # Change directory to run command
+            os.system('start.')  # Run command
+            os.chdir(opd)  # Return to original directory
 
 
 async def copyFiles(src_files, dest_dir):
@@ -127,14 +178,49 @@ currentFilePath = Label(
     root, textvariable=selectedFilePath, background='#006778', foreground='#FFD124', anchor=W)
 currentFilePath.place(x=5, y=50, width=490, height=20)
 
+inputPathBoxLabel = LabelFrame(root, text="Input file path",
+                               background='#006778', foreground='#FFD124', padx=5, pady=5)
+inputPathBoxLabel.place(x=5, y=75, width=490, height=20)
+
+inputPathBox = scrolledtext.ScrolledText(
+    root, background='#006778', foreground='#FFD124')
+inputPathBox.place(x=5, y=95, width=490, height=40)
+
+inputPathBox.config(state="disabled")
+
+outputPathBoxLabel = LabelFrame(root, text="Output file path",
+                                background='#006778', foreground='#FFD124', padx=5, pady=5)
+outputPathBoxLabel.place(x=5, y=140, width=490, height=20)
+
+outputPathBox = scrolledtext.ScrolledText(
+    root, background='#006778', foreground='#FFD124')
+outputPathBox.place(x=5, y=160, width=490, height=40)
+
+outputPathBox.config(state="disabled")
+
 madeWithLoveBy = Label(
     root, text='Made with ❤ by Mohamed Omar', background='#006778', foreground='#FFD124', font=('monospace', 9, 'bold'))
-madeWithLoveBy.place(x=310, y=180, width=190, height=20)
+madeWithLoveBy.place(x=310, y=230, width=190, height=20)
+
+m = Menu(root, tearoff=0)
+m.add_command(label="Select All", command=selectAll)
+m.add_command(label="Copy", command=copy)
+
+
+def do_popup(event):
+    try:
+        m.tk_popup(event.x_root, event.y_root)
+    finally:
+        m.grab_release()
+
+
+inputPathBox.bind("<Button-3>", do_popup)
+outputPathBox.bind("<Button-3>", do_popup)
 
 setButtonsDisabled()
 
 root.title('GDR_Handler')
-root.geometry('500x200')
+root.geometry('500x250')
 root.configure(bg='#000')
 
 root.resizable(False, False)
